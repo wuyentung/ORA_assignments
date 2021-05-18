94b83a313341c093af606f271d0dc7e5a68880c4
wuyentung <wuyentung@gmail.com>
2021-05-18 12:01:05 +0800

    01

--- 04/01.py
+++ 04/01.py
@@ -3,3 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gurobipy as gp
 #%%
+w_method=gp.Model("VRS_model")
+vrs_v[r,i]=w_method.addVar(vtype=gp.GRB.CONTINUOUS,name="%s"%(INPUTS[i]))
+w_method.update()
+w_method.setObjective(gp.quicksum(vrs_u[r,j]*Y[r][j] for j in range(O))-u0[r],gp.GRB.MAXIMIZE)
+w_method.addConstr(gp.quicksum(vrs_v[r,i]*X[r][i] for i in range(I))==1)
+w_method.optimize()
+record[r] = np.round([v.x for v in w_method.getVars()] + [w_method.objVal] + [crs_model.objVal], 6)
